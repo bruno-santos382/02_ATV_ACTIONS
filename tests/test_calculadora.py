f24e2d37c4400f6c10928dbc701a8bbfb9628e5c
bruno.santos382 <bruno.santos382@fatec.sp.gov.br>
2025-09-20 12:01:27 -0300

    Implementar subtração e multiplicação

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,7 +1,7 @@
 # tests/test_calculadora.py
 
 import pytest
-from src.calculadora import somar, dividir
+from src.calculadora import somar, dividir, multiplicar, subtrair
 
 
 def test_somar():
@@ -14,3 +14,11 @@ def test_dividir():
     with pytest.raises(ValueError):
         dividir(10, 0)
 
+def test_multiplicar():
+    assert multiplicar(2, 3) == 6
+    assert multiplicar(-1, 1) == -1
+
+
+def test_subtrair():
+    assert subtrair(5, 3) == 2
+    assert subtrair(-1, 1) == -2
